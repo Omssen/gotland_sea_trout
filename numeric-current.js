f2ec0024485e3c9ece0c971a4360fe8b512a1ed4
Omssen <andreas.sinning@gmx.com>
2026-09-13 12:20:37 +0200

    Add numeric grid file loader

--- numeric-current.js
+++ numeric-current.js
@@ -1,2 +1,6 @@
 /* Gotland Sea Trout v6.2.4 numeric current test */
+async function loadNumericGridFile(){
+ const response=await fetch('data/current/latest.json');
+ return response.json();
+}
 console.info('v6.2.4 numeric current module loaded');
